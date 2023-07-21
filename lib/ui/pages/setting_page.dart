2c2803fb5a87debb1a019ea8032f09d4e97ffd66
achmadsm <achmad.10120701@mahasiswa.unikom.ac.id>
2023-07-21 09:59:35 +0700

    Add sign in

--- lib/ui/pages/setting_page.dart
+++ lib/ui/pages/setting_page.dart
@@ -1,4 +1,5 @@
 import 'package:airplane/cubit/auth_cubit.dart';
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/ui/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -20,9 +21,10 @@ class SettingPage extends StatelessWidget {
             ),
           );
         } else if (state is AuthInitial) {
+          context.read<PageCubit>().setPage(0);
           Navigator.pushNamedAndRemoveUntil(
             context,
-            '/sign-up',
+            '/sign-in',
             (route) => false,
           );
         }
