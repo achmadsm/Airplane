3e6335ecf43a0de71f730728368e69d6bedf1947
achmadsm <achmad.10120701@mahasiswa.unikom.ac.id>
2023-07-21 14:04:19 +0700

    Add portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,14 @@ import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
